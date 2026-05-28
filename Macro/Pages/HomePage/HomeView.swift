c2c7e68dcdf7ce510fcb00c79ac385eb7587d233
shrey <shrey.gangwar21@gmail.com>
2026-05-27 22:47:04 -0700

    Implement Home Page Navigation Bar Placeholder

--- Macro/Pages/HomePage/HomeView.swift
+++ Macro/Pages/HomePage/HomeView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct HomeView: View {
+
+    func currDate() -> String {
+        let date = Date()
+        return date.formatted(.dateTime.weekday(.wide).month(.wide).day())
+    }
+
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Hello World!")
+            }
+            .navigationTitle("Home")
+            .navigationSubtitle(currDate())
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button {
+                    } label: {
+                        Image(systemName: "calendar")
+                    }
+
+                    Button {
+                    } label: {
+                        Image(systemName: "gear")
+                    }
+
+                    Menu {
+                        Button {
+                        } label: {
+                            Label(
+                                "Import...",
+                                systemImage: "square.and.arrow.down"
+                            )
+                        }
+
+                        Divider()
+
+                        Button(role: .destructive) {
+                        } label: {
+                            Label(
+                                "Delete All",
+                                systemImage: "trash",
+
+                            )
+                        }
+
+                    } label: {
+                        Image(systemName: "ellipsis")
+                    }
+                }
+            }
+        }
     }
 }
 
